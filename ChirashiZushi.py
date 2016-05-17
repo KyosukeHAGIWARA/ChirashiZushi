9ac282184a90efb70295688ab320c7941b0a1582
KyosukeHagiwara <s1311388@coins.tsukuba.ac.jp>
2016-05-17 15:36:34 +0900

    refactor main() and add get_chirashi_url()

--- ChirashiZushi.py
+++ ChirashiZushi.py
@@ -17,9 +17,35 @@ import ConfigParser
 
 now = datetime.now().strftime("%s")
 parent = "./data/" + now
-os.makedirs(parent)
 
+shop_name = {
+    "kasumi" : "カスミ テクノパーク桜店",
+    "marumo" : "マルモ学園店",
+    "aeon" : "イオンつくば駅前店",
+}
 
+#scrape HP and get chirashi url
+def get_chirashi_url(shop):
+    chirashis = []
+    
+    if shop=="kasumi":
+        html = open("./data/kasumi_sakura.html", "r").read()
+        soup = BeautifulSoup(html, "lxml")
+        for chirashi in soup.select("#chirashiList1")[0].children:
+            c_id = chirashi.get("id")
+            c_scheme = chirashi.select(".shufoo-scheme")[0].contents[0]
+            c_url = chirashi.select(".shufoo-pdf")[0].a.get("href")
+            c_data = {
+                "url" : c_url,
+                "scheme" : c_scheme,
+                "id" : c_id,
+            }
+            chirashis.append(c_data)
+    elif shop=="marumo":
+        pass
+    elif shop=="aeon":
+        pass
+    return chirashis
 
 # Scraping KASUMI and get Chirashi Data
 def chirashi_search():
@@ -34,32 +60,55 @@ def chirashi_search():
         chirashis.append(c_data)
     return chirashis
 
-# generate pdf data from redirect URL
-def gen_chirashi_pdf(c_data, parent_path):
-    target = c_data[2]
-    red = urllib2.urlopen(target).read()
-    red_soup = BeautifulSoup(red, "lxml")
-    url = red_soup.meta.get("content").lstrip("0;URL=")
-    path = parent_path + "/" + re.findall(r"[0-9]+", c_data[0])[0] + ".pdf"
-    if subprocess.call(["python", "-m", "wget", "-o", path, url]) != 0:
-                    print "failed: {0}".format(url)
-    return path
 
-# convert Chirashi pdf to png
-def pdf_to_png(root_path):
-    for dirpath, _, filenames in os.walk(root_path):
-        for filename in filenames:
-            if fnmatch.fnmatch(filename, u"*.pdf"):
-                org_path = os.path.join(dirpath, filename)
-                ppp = dirpath
-                ppp_path = os.path.join(ppp, filename)
-                png_path = ppp_path.replace(".pdf", ".png")
-                print(png_path)
+def gen_chirashi_pdf(url, dirpath, outname):
+    pass
 
-                print "convert {0} to {1}".format(org_path, png_path)
+def pdf_to_png(dirpath):
+    pass
 
-                if subprocess.call(["convert", "-density", "130", "-trim", org_path, png_path]) != 0:
-                    print "failed: {0}".format(org_path)
+def chirath(dirpath, shop, scheme):
+    pass
+
+# # Scraping KASUMI and get Chirashi Data
+# def chirashi_search():
+#     dat = open("./data/kasumi_sakura.html", "r").read()
+#     kasumi_soup = BeautifulSoup(dat, "lxml")
+#     chirashis = []
+#     for chirashi in kasumi_soup.select("#chirashiList1")[0].children:
+#         c_id = chirashi.get("id")
+#         c_scheme = chirashi.select(".shufoo-scheme")[0].contents[0]
+#         c_url = chirashi.select(".shufoo-pdf")[0].a.get("href")
+#         c_data = (c_id, c_scheme, c_url)
+#         chirashis.append(c_data)
+#     return chirashis
+
+# # generate pdf data from redirect URL
+# def gen_chirashi_pdf(c_data, parent_path):
+#     target = c_data[2]
+#     red = urllib2.urlopen(target).read()
+#     red_soup = BeautifulSoup(red, "lxml")
+#     url = red_soup.meta.get("content").lstrip("0;URL=")
+#     path = parent_path + "/" + re.findall(r"[0-9]+", c_data[0])[0] + ".pdf"
+#     if subprocess.call(["python", "-m", "wget", "-o", path, url]) != 0:
+#                     print "failed: {0}".format(url)
+#     return path
+
+# # convert Chirashi pdf to png
+# def pdf_to_png(root_path):
+#     for dirpath, _, filenames in os.walk(root_path):
+#         for filename in filenames:
+#             if fnmatch.fnmatch(filename, u"*.pdf"):
+#                 org_path = os.path.join(dirpath, filename)
+#                 ppp = dirpath
+#                 ppp_path = os.path.join(ppp, filename)
+#                 png_path = ppp_path.replace(".pdf", ".png")
+#                 print(png_path)
+
+#                 print "convert {0} to {1}".format(org_path, png_path)
+
+#                 if subprocess.call(["convert", "-density", "130", "-trim", org_path, png_path]) != 0:
+#                     print "failed: {0}".format(org_path)
 
 
 # return twitter oath
@@ -89,22 +138,30 @@ def chirath(root_path, scheme):
             reply_id = None
 
 
-# def chirath_test(filename, text=None, reply_id=None):
-#     auth = get_oauth()
-#     api = API(auth)
-#     st = api.update_with_media(filename=filename, status="aaa", reply_id=reply_id)
-
 if __name__ == '__main__':
-    chirashis = chirashi_search()
-    for chirashi in chirashis:
-        now = datetime.now().strftime("%s")
-        p_path = parent + "/" + now
-        os.makedirs(p_path)
+    os.makedirs(parent)
+    for shop in shop_name.keys():
+        shopdir = parent + "/" + shop
+        os.makedirs(shopdir)
+        for i, chirashi in enumerate(get_chirashi_url(shop)):
+            currentdir = shop + str(i)
+            os.makedirs(currentdir)
+            outname = currentdir + ".pdf"
+            gen_chirashi_pdf(chirashi["url"], currentdir, outname)
+            sleep(5)
+            pdf_to_png(currentdir)
+            sleep(10)
+            chirath(currentdir, shop, chirashi["scheme"])
 
-        gen_chirashi_pdf(chirashi, p_path)
-        sleep(5)
-        pdf_to_png(p_path)
-        sleep(10)
-        chirath(p_path, chirashi[1])
-    # chirath_test("./data/icon.jpeg")
+    # chirashis = chirashi_search()
+    # for chirashi in chirashis:
+    #     now = datetime.now().strftime("%s")
+    #     p_path = parent + "/" + now
+    #     os.makedirs(p_path)
+    #     gen_chirashi_pdf(chirashi, p_path)
+    #     sleep(5)
+    #     pdf_to_png(p_path)
+    #     sleep(10)
+    #     chirath(p_path, chirashi[1])
+    # # chirath_test("./data/icon.jpeg")
  
